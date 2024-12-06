7b20ddae3b172db108a87ad2904db67d5857f9d3
[adrimarieg] <adriannegonzalez@aol.com>
2024-12-06 10:20:44 -0500

    update name and local parent folder

--- base-64.js
+++ base-64.js
@@ -8,15 +8,8 @@ hex string to base64. For example, the string: deadbeef
 should produce: 3q2+7w==
 */
 
-function convertToBaseSixFour(string){
-    //convert string to binary data
-    let byteArray = new Uint8Array(string.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
 
-    //convert binary data to base64
-    let base64String = btoa(String.fromCharCode(...byteArray));
-
-    return base64String;
-};
-
-console.log(convertToBaseSixFour("deadbeef"))
-
+funtion hexToBase(hexStr) {
+    //convert hex string to binary data (Uint8Array)
+    const binaryData = Uint8Array.from()
+}
